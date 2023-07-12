176334d7ba0984788e20ce706e6fdb4efa5776fd
MinhDuy17072002 <duy.207ct68618@vanlanguni.vn>
2023-07-12 12:06:33 +0700

    Initial commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'shopping.wlsymg8.mongodb.net',
+  DB_USER: 'shopping-online',
+  DB_PASS: '0918436433',
+  DB_DATABASE: 'Shopping-online',
+  EMAIL_USER: 'minhduy00007@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'skprpxfgpuddqgkd',
+  JWT_SECRET: '<jwt_secret>',
+  JWT_EXPIRES: '60000000000', // in milliseconds
+};
+module.exports = MyConstants;
